0d4dec915f3c441200e4430f905027d8fd186433
serceserbatic <serce@serbatic.es>
2024-05-10 12:32:56 +0200

    version funcional

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-form',
@@ -60,6 +63,137 @@ export class FormComponent {
   }
 
   exportToPDF() {
-    console.log(this.cvForm.value);
+    const sectionArrayValues = this.sectionArray.getRawValue();
+
+    const pdfDefinition: any = {
+      content: [
+        {
+          alignment: 'justify',
+          columns: [
+            {
+              width: 150,
+              text: "serbatic",
+              style: 'header'
+            },
+            {
+              text: this.cvForm.get('firstName').value + " " + this.cvForm.get('surname1').value + " " + this.cvForm.get('surname2').value + " " + " V1 - Developer"
+            }
+          ]
+        },
+        '\n',
+        {
+          alignment: 'justify',
+          columns: [
+            {
+              width: 150,
+              text: "Datos Profesionales",
+              style: 'header'
+            },
+            {
+              text: 'Consejo: ' + this.cvForm.get('board').value +
+                '\nClan: ' + this.cvForm.get('clan').value +
+                '\nOficina: ' + this.cvForm.get('office').value +
+                '\nPuesto: ' + this.cvForm.get('position').value +
+                '\nFecha de alta: ' + this.cvForm.get('professionalStart').value
+            }
+          ]
+        },
+        '\n',
+        {
+          alignment: 'justify',
+          columns: [
+            {
+              width: 150,
+              text: "Experiencia en grupo VASS",
+              style: 'header'
+            },
+            {
+              stack: sectionArrayValues.map((item: any) => {
+                return [
+                  'Fecha de inicio: ' + item.vassStart,
+                  'Fecha de fin: ' + item.vassEnd,
+                  'Puesto: ' + item.vassPossition,
+                  'Otro Puesto: ' + item.vassOtherPossition,
+                  'Cliente: ' + item.client,
+                  'Proyecto: ' + item.project,
+                  'Descripción:' + item.description,
+                  "\n"
+                ];
+              })
+            }
+          ]
+        }, '\n',
+        {
+          alignment: 'justify',
+          columns: [
+            {
+              width: 150,
+              text: "Formación académica",
+              style: 'header'
+            },
+            {
+              text: 'Titulación: ' + this.cvForm.get('degree').value +
+                '\nTipo de titulación: ' + this.cvForm.get('typeDegree').value +
+                '\nFecha de inicio: ' + this.cvForm.get('academicStart').value +
+                '\nFecha fin: ' + this.cvForm.get('academicEnd').value +
+                '\nCentro: ' + this.cvForm.get('institution').value
+            }
+          ]
+        }, '\n',
+        {
+          alignment: 'justify',
+          columns: [
+            {
+              width: 150,
+              text: "Idiomas",
+              style: 'header'
+            },
+            {
+              text: this.cvForm.get('languages').value
+            }
+          ]
+        }, '\n',
+        {
+          alignment: 'justify',
+          columns: [
+            {
+              width: 150,
+              text: "Conocimientos tecnológicos",
+              style: 'header'
+            },
+            {
+              text: this.cvForm.get('skills').value
+            }
+          ]
+        }, '\n',
+        {
+          alignment: 'justify',
+          columns: [
+            {
+              width: 150,
+              text: "Certificaciones",
+              style: 'header'
+            },
+            {
+              text: this.cvForm.get('certifications').value
+            }
+          ]
+        },
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          alignment: 'right',
+        }
+      },
+      defaultStyle: {
+        columnGap: 20
+      }
+    }
+
+    const pdf = pdfMake.createPdf(pdfDefinition);
+
+    pdf.open();
   }
 }
